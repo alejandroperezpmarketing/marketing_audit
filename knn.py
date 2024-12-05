077c7d90b461d4879b1cff202fe02815a83dfcc9
Alejandro Perez <alejandro@example.com>
2024-12-05 10:21:42 +0000

    Initial commit

--- knn.py
+++
@@ -1,8 +0,0 @@
-import numpy as np 
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix 
-
-from time import time
-import matplotlib.pyplot as plt
